21690100d8f566d5224d44754177e87b5313a82b
“Luizmar <luizmarcardozo@gmail.com>
2024-06-07 15:07:44 -0300

    Commit inicial

---
+++ src/main/java/com/placeti/placeti/PlacetiApplication.java
@@ -0,0 +1,13 @@
+package com.placeti.placeti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacetiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacetiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/placeti/placeti/PlacetiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placeti.placeti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacetiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
